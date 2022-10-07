a0e6cdb5d0d5bdfae1d0beb39a256f057adf604a
Tùng Trần <59041428+tungtv289@users.noreply.github.com>
2022-10-08 01:46:13 +0700

    bai tap doc file

---
+++ array/init_arrray_using_read_file.php
@@ -0,0 +1,4 @@
+1. khởi tạo mảng DoAn(name, price, status, ownerm, type) bằng cách đọc từ fiel doan10.txt
+2. sửa đồ ăn có tên là "Valli" thành giá là 100000
+3. sửa tất cả đồ ăn có type = 3 thành giá là 5000
+4. sửa tất cả đồ ăn có status=0 thành giá là 0
