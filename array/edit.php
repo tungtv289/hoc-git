fac94652faa59d268bd8505793031276ed7d4922
Tùng Trần <59041428+tungtv289@users.noreply.github.com>
2022-10-08 00:58:27 +0700

    tungtv giao bai tap

---
+++ array/edit.php
@@ -0,0 +1,4 @@
+// bai tap
+1. tạo khởi tạo mảng có kiểu là DoAn(name, price, status, ownerm, type) với 5 phần tử ban đầu 
+2. sửa đồ ăn có tên là "doan1" thành giá là 100000
+3. sửa tất cả đồ ăn có type = 1 thành giá là 5000
