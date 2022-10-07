a2fbb3d1f08a376d10c975793d52087c7595c209
Your Name01~ <you@example.com>
2022-10-08 02:15:21 +0700

    da hoan thanh bai 1

--- array/init_arrray_using_read_file.php
+++ array/init_arrray_using_read_file.php
@@ -1,4 +1,42 @@
-1. khởi tạo mảng DoAn(name, price, status, ownerm, type) bằng cách đọc từ fiel doan10.txt
+<!-- 1. khởi tạo mảng DoAn(name, price, status, ownerm, type) bằng cách đọc từ fiel doan10.txt
 2. sửa đồ ăn có tên là "Valli" thành giá là 100000
 3. sửa tất cả đồ ăn có type = 3 thành giá là 5000
-4. sửa tất cả đồ ăn có status=0 thành giá là 0
+4. sửa tất cả đồ ăn có status=0 thành giá là 0 -->
+
+<?php
+
+use DoAn as GlobalDoAn;
+
+class DoAn
+{
+    public $id;
+    public $name;
+    public $price;
+    public $type;
+    public $status;
+
+    public function __construct( $id, $name, $price,$type,$status)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+        $this->type = $type;
+        $this->status = $status;
+    }
+}
+$myfile = fopen("doan10.txt", "r") or die("Unable to open file!");
+$noidungfile= fread($myfile, filesize("doan10.txt"));
+fclose($myfile);
+$mang1 = explode(PHP_EOL, $noidungfile);
+foreach ($mang1 as $value) {
+    echo $value . "\n";
+    $tmpid = explode(",", $value)[0];
+    $tmpname = explode(",", $value)[1];
+    $tmpprice = explode(",", $value)[2];
+    $tmptype = explode(",", $value)[3];
+    $tmpstatus = explode(",", $value)[4];
+    $obj = new DoAn ($tmpid, $tmpname, $tmpprice,$tmptype ,$tmpstatus);
+    $DoanArr[] = $obj;
+  }
+
+?>
